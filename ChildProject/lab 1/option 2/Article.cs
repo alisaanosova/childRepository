ce01587e0fa72da4590c01ad2063a6536f22456d
alisaanosova <alisaanosova@mail.ru>
2017-03-22 00:55:43 +0200

    Update Article.cs

--- ChildProject/lab 1/option 2/Article.cs
+++ ChildProject/lab 1/option 2/Article.cs
@@ -8,6 +8,9 @@ namespace ChildProject.lab_1.option_2
 {
     class Article
     {
+        //лучше не называть переменную именем класса тем более в одной строчке. не ошибка. но стиль кода не хороший.
+        //это было бы допустимо с переменной, потому что ты просто с маленькой буквы пишешь и не юзаешь ее много раз
+        // но тут не очень хорошо
         public Person Person { set; get; }
         public string Name { set; get; }
         public double Raiting { set; get; }
