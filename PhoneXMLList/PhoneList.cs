a88990e6e682e1b09561e3b5ac69583f93bf4d44
happycv <gammerok1@mail.ru>
2017-05-05 14:38:37 +0300

    add new project PhoneXMLList

---
+++ PhoneXMLList/PhoneList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PhoneXMLList
+{
+    class PhoneList
+    {
+
+
+            private readonly string _fileAdress;
+            public PhoneList(string fileAddress)
+            {
+                XDocument phone = new XDocument();
+                XElement phones = new XElement("items");
+                phone.Add(phones);
+                _fileAdress = fileAddress;
+                phone.Save(_fileAdress + ".xml");
+            }
+
+            public void PhonesAdd(string phoneModel, string phoneComp, int phonePrice)
+            {
+                XDocument xDoc = XDocument.Load(_fileAdress + ".xml");
+                XElement root = xDoc.Element("items");
+
+                root.Add(new XElement("phone",
+                    new XAttribute("name", phoneModel),
+                    new XElement("company", phoneComp),
+                    new XElement("price", $"{phonePrice}")));
+                xDoc.Save(_fileAdress + ".xml");
+            }
+
+            public void PhoneListShow()
+            {
+                XDocument xdoc = XDocument.Load(_fileAdress + ".xml");
+                foreach (XElement phoneElement in xdoc.Element("items").Elements("phone"))
+                {
+                    XAttribute nameAttribute = phoneElement.Attribute("name");
+                    XElement companyElement = phoneElement.Element("company");
+                    XElement priceElement = phoneElement.Element("price");
+
+                    if (nameAttribute != null && companyElement != null && priceElement != null)
+                    {
+                        Console.WriteLine("SmartPhone: {0}", nameAttribute.Value);
+                        Console.WriteLine("Company: {0}", companyElement.Value);
+                        Console.WriteLine("Price: {0}", priceElement.Value);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+}
---
+++ PhoneXMLList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneXMLList
+{
+    class Phone
+    {
+        public string PhoneModel { get; set; }
+        public string PhoneComp { get; set; }
+        public int Price { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PhoneList list = new PhoneList("PhonesList");
+            List<Phone> phones = new List<Phone>();
+            phones.Add(new Phone() { PhoneModel = "a3310", PhoneComp = "nokia", Price = 4000 });
+            phones.Add(new Phone() { PhoneModel = "a3310", PhoneComp = "nokia", Price = 4000 });
+            phones.Add(new Phone() { PhoneModel = "a3310", PhoneComp = "nokia", Price = 4000 });
+            foreach (Phone p in phones)
+            {
+                list.PhonesAdd(p.PhoneModel, p.PhoneComp, p.Price);
+            }
+            list.PhoneListShow();
+            Console.ReadKey();
+        }
+    }
+}
